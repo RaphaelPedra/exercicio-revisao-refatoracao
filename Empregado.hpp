0a4fd2132c9038d257cc1972c386348aacc1e1eb
RaphaelPedra <73200487+RaphaelPedra@users.noreply.github.com>
2020-10-20 18:33:43 -0300

    Update Empregado.hpp

--- Empregado.hpp
+++ Empregado.hpp
@@ -1,28 +1,27 @@
 #ifndef EMPREGADO_H
 #define EMPREGADO_H
-
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 class Empregado {
-	
+
   public:
-    double salarioHora;  
-    double quotaMensalVendas;  
+    Empregado();
+    double setsalarioHora(double salarioHora);
+    double getsalarioHora();
+    const static int horasFixas = 8;
+    double horasTrabalhadas;
+    string setnome(string nome);
+    string getnome();
+    double pagamentoMes(double horasTrabalhadas);
 
 
-    double pagamentoMes(double horasTrabalhadas) {
- 
-      double t = horasTrabalhadas;
-	  
-	  //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
-      if (horasTrabalhadas > 8) {
-        double x = horasTrabalhadas - 8;
-        t += x / 2;
-      }
-	  return t * salarioHora;
-    }
-	
+  private:
+      string nome;
+      double salarioHora;
+
 };
 
 #endif
