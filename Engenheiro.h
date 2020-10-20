0b2ac808993ae46f406a9d04b2a8f2a5fcd8bbed
RaphaelPedra <73200487+RaphaelPedra@users.noreply.github.com>
2020-10-20 20:09:37 -0300

    Create Engenheiro.h

---
+++ Engenheiro.h
@@ -0,0 +1,13 @@
+#include "Engenheiro.h"
+
+Engenheiro::Engenheiro(){
+}
+
+int Engenheiro::setprojetos(int projetos){
+    this->projetos = projetos;
+}
+
+int Engenheiro::getprojetos(){
+    return projetos;
+}
+
